2a52673b87fcb073d2abce528a1d221bea493cf8
ss16sr <sophiarobertson@gmail.com>
2020-04-08 18:16:06 +0100

    study page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+const myForm = document.getElementById("myForm");
 
+myForm.addEventListener("submit", (addEventListener) => {
+e.preventDefault();
+
+console.log("Form has been submitted!")
+});
---
+++ main.js
@@ -0,0 +1,8 @@
+const myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", (e) => {
+e.preventDefault();
+
+console.log("Form has been submitted");
+});
+
